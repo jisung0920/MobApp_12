5a3dc88c4fd9a777495841955bbdefa874267785
jisung <jisung0920@gmail.com>
2017-05-18 13:12:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jisung/mobapp_12/PracActivity.java
@@ -0,0 +1,13 @@
+package com.example.jisung.mobapp_12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prac);
+    }
+}
---
+++ app/src/main/java/com/example/jisung/mobapp_12/S_Activity.java
@@ -0,0 +1,25 @@
+package com.example.jisung.mobapp_12;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class S_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_);
+    }
+    void onClick(View v){
+        if(v.getId()==R.id.bt1){
+            Intent intent = new Intent(this,PracActivity.class);
+            startActivity(intent);
+        }/*
+        else{
+            Intent intent = new Intent(this,Main2Activity.class);
+            startActivity(intent);
+        }*/
+    }
+}
