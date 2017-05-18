5e1a4c8caf2767fb0460619f8fe39318851bc534
jisung <jisung0920@gmail.com>
2017-05-18 17:15:27 +0900

    블러 에러랑 파일입출력 남음

--- app/src/main/java/com/example/jisung/mobapp_12/MyDraw.java
+++ app/src/main/java/com/example/jisung/mobapp_12/MyDraw.java
@@ -3,8 +3,12 @@ package com.example.jisung.mobapp_12;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.DrawFilter;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -19,60 +23,132 @@ public class MyDraw extends View {
     Bitmap mBit;
     Canvas mCan;
     Paint mPaint = new Paint();
-    int oldX=-1,oldY=-1;
+    Boolean dmode = true;
+    BlurMaskFilter blur;
+
+    int oldX = -1, oldY = -1;
+
     public MyDraw(Context context) {
         super(context);
+        this.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         mPaint.setColor(Color.BLACK);
+
     }
 
     public MyDraw(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mPaint.setColor(Color.BLACK);
+
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 
 
-        mBit = Bitmap.createBitmap(w,h, Bitmap.Config.ARGB_8888);
+        mBit = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         mCan = new Canvas();
         mCan.setBitmap(mBit);
         mCan.drawColor(Color.YELLOW);
 
-        drawStamp();
+        drawStamp(1, 1);
         super.onSizeChanged(w, h, oldw, oldh);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if(mBit!=null)
-        canvas.drawBitmap(mBit,0,0,mPaint);
+        if (mBit != null)
+            canvas.drawBitmap(mBit, 0, 0, mPaint);
     }
-    private void drawStamp(){
-        Bitmap img = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
-        mCan.drawBitmap(img,10,10,mPaint);
+
+    private void drawStamp(int x, int y) {
+        Bitmap img = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        mCan.drawBitmap(img, x, y, mPaint);
+    }
+
+    public void clear() {
+        mBit.eraseColor(Color.WHITE);
+        invalidate();
+    }
+
+    public void Blur(Boolean check) {
+        if (check) {
+            blur = new BlurMaskFilter(80, BlurMaskFilter.Blur.INNER);
+            mPaint.setMaskFilter(blur);
+        } else {
+            mPaint = new Paint();
+        }
+        invalidate();
+    }
+
+    public void ColorF(Boolean check) {
+
+        if (check) {
+            float[] ary = {2f, 0f, 0f, 0f, -25f,
+                    0f, 2f, 0f, 0f, -25f,
+                    0f, 0f, 2f, 0f, -25f,
+                    0f, 0f, 0f, 1f, 0f,
+            };
+            ColorMatrix matrix = new ColorMatrix(ary);
+            mPaint.setColorFilter(new ColorMatrixColorFilter(matrix));
+        }
+        else
+            mPaint=new Paint();
+        invalidate();
+    }
+
+    public void textSet(int s) {
+        mPaint.setStrokeWidth(s);
+
+    }
+
+    public void testColor(int i) {
+        if (i == 1)
+            mPaint.setColor(Color.RED);
+        else
+            mPaint.setColor(Color.BLUE);
+    }
+
+    public void rotateSet() {
+        mCan.rotate(30, this.getWidth() / 2, this.getWidth() / 2);
+
+    }
+
+    public void moveSet() {
+        mCan.translate(10f, 10f);
+    }
+
+    public void scaleSet() {
+        mCan.scale(1.5f, 1.5f);
+    }
+
+    public void skewSet() {
+        mCan.skew(0.2f, 0);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        int X = (int)event.getX();
-        int Y = (int)event.getY();
-        if(event.getAction()==MotionEvent.ACTION_DOWN){
+        int X = (int) event.getX();
+        int Y = (int) event.getY();
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (!dmode)
+                drawStamp(X, Y);
+            invalidate();
+
             oldX = X;
             oldY = Y;
-        }
-        else if(event.getAction()==MotionEvent.ACTION_MOVE){
-            if(oldX!=-1) {
-                mCan.drawLine(oldX, oldY, X, Y, mPaint);
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            if (oldX != -1) {
+                if (dmode)
+                    mCan.drawLine(oldX, oldY, X, Y, mPaint);
                 invalidate();
                 oldX = X;
                 oldY = Y;
             }
-        }
-        else if(event.getAction()==MotionEvent.ACTION_UP){
-            if(oldX!=-1) {
-                mCan.drawLine(oldX, oldY, X, Y, mPaint);
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (oldX != -1) {
+                if (dmode)
+                    mCan.drawLine(oldX, oldY, X, Y, mPaint);
                 invalidate();
             }
             oldX = -1;
--- app/src/main/java/com/example/jisung/mobapp_12/PaintActivity.java
+++ app/src/main/java/com/example/jisung/mobapp_12/PaintActivity.java
@@ -1,21 +1,39 @@
 package com.example.jisung.mobapp_12;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class PaintActivity extends AppCompatActivity {
 
+    MyDraw screen;
+    CheckBox c1;
+    int bntCount=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paint);
+        screen = (MyDraw)findViewById(R.id.mview);
+        c1= (CheckBox)findViewById(R.id.c1);
+        c1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    screen.dmode=false;
+                else
+                    screen.dmode=true;
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         menu.add(0,1,0,"Blurring").setCheckable(true);
-        menu.add(0,2,0,"Coloring").setChecked(true);
+        menu.add(0,2,0,"Coloring").setCheckable(true);
         menu.add(0,3,0,"Pen Width BIG").setCheckable(true);
         menu.add(0,4,0,"Pen Color RED");
         menu.add(0,5,0,"pen Color BLUE");
@@ -27,21 +45,66 @@ public class PaintActivity extends AppCompatActivity {
         if(item.getItemId()==1){
             if(item.isChecked()){
                 item.setChecked(false);
+                screen.Blur(false);
             }
             else{
                 item.setChecked(true);
+                screen.Blur(true);
             }
         }
         else if(item.getItemId()==2){
+            if(item.isChecked()){
+                item.setChecked(false);
+                screen.ColorF(false);
+            }
+            else{
+                item.setChecked(true);
+                screen.ColorF(true);
+            }
 
         }
         else if(item.getItemId()==3){
+            if(item.isChecked()){
+                item.setChecked(false);
+                screen.textSet(3);
+            }
+            else{
+                item.setChecked(true);
+                screen.textSet(5);
+
+            }
 
         }
-        else if(item.getItemId()==4){}
-        else if(item.getItemId()==5){}
+        else if(item.getItemId()==4){
+            screen.testColor(1);
+        }
+        else if(item.getItemId()==5){
+            screen.testColor(2);
+        }
 
 
-        return true;
+        return false;
+    }
+    void onClick(View v){
+        if(v.getId()==R.id.erase){
+            screen.clear();
+        }
+        else if(v.getId()==R.id.rot){
+            c1.setChecked(true);
+            screen.rotateSet();
+        }
+        else if(v.getId()==R.id.mov)
+        {
+            c1.setChecked(true);
+            screen.moveSet();
+        }
+        else if(v.getId()==R.id.sca){
+            c1.setChecked(true);
+            screen.scaleSet();
+        }
+        else if(v.getId()==R.id.ske){
+            c1.setChecked(true);
+            screen.skewSet();
+        }
     }
 }
